f88d4ebff59babe84f77cb4cf7b039a67bf0f274
Amaytics <mac@Admins-MacBook-Air.local>
2026-02-06 15:48:54 +0530

    js revision

---
+++ day-11/script.js
@@ -0,0 +1,20 @@
+// function handleClick(){
+//     document.querySelector("#head").style.color="red";
+
+//     document.getElementById("btn").textContent="color changed"
+
+// }
+
+// const handleClick = () => {
+//     const image = document.createElement("img");
+//     image.setAttribute("src","https://www.google.com/imgres?q=kr%20mangalam&imgurl=https%3A%2F%2Fwww.krmangalam.edu.in%2Fwp-content%2Fuploads%2F2024%2F02%2F183bs_Top-Colleges-for-Fashion-Design-Program-In-Gurgaon-768x432-1.webp&imgrefurl=https%3A%2F%2Fkrmangalam.edu.in%2Fblog%2Fkr-mangalam-educational-excellence-redefined&docid=kADqyjaGBMtJhM&tbnid=ZFK4xcWVjxmXGM&vet=12ahUKEwjlqtr_zMSSAxWSSGcHHYtIKu0QnPAOegQIExAB..i&w=768&h=432&hcb=2&ved=2ahUKEwjlqtr_zMSSAxWSSGcHHYtIKu0QnPAOegQIExAB")
+//     image.setAttribute("alt","kr mangalam");
+//     document.getElementById("content").append(image);
+// }
+
+// const handleClick = () => {
+//     const image = document.createElement("img");
+//     image.setAttribute("src","https://www.google.com/imgres?q=kr%20mangalam&imgurl=https%3A%2F%2Fwww.krmangalam.edu.in%2Fwp-content%2Fuploads%2F2024%2F02%2F183bs_Top-Colleges-for-Fashion-Design-Program-In-Gurgaon-768x432-1.webp&imgrefurl=https%3A%2F%2Fkrmangalam.edu.in%2Fblog%2Fkr-mangalam-educational-excellence-redefined&docid=kADqyjaGBMtJhM&tbnid=ZFK4xcWVjxmXGM&vet=12ahUKEwjlqtr_zMSSAxWSSGcHHYtIKu0QnPAOegQIExAB..i&w=768&h=432&hcb=2&ved=2ahUKEwjlqtr_zMSSAxWSSGcHHYtIKu0QnPAOegQIExAB")
+//     image.setAttribute("alt","kr mangalam");
+//     document.getElementById("content").append(image);
+// }
