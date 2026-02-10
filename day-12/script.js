7e1d5d5599a662d0b9c83626c0f8db299df699d3
Amaytics <mac@Admins-MacBook-Air.local>
2026-02-10 10:14:48 +0530

    name

---
+++ day-12/script.js
@@ -0,0 +1,14 @@
+// document.getElementById("myInput").addEventListener("change",()=>{
+//     document.getElementById("output").textContent ="Change detected"
+// })
+
+// const nameInput=document.getElementById("myInput");
+// nameInput.addEventListener("change",()=>{
+
+// })
+
+const nameInput = document.getElementById("myInput");
+const courseInput=document.getElementById("course");
+const btn=document.getElementById("btn");
+const output=document.getElementById("output");
+
